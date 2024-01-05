b264f5dcd4dae402f5943b5afea2be5264afde26
Stacy Griffy <sgriffy2@gmail.com>
2024-01-05 09:50:18 -0600

    Initial Commit

---
+++ scripts/CityList.js
@@ -0,0 +1,27 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const cityTarget = clickEvent.target
+
+        if (cityTarget.dataset.type === "city") {
+            window.alert(`${cityTarget.dataset.walkername} is servicing this city`)
+        }
+    }
+)
+
+export const CityList = () => {
+    let citiesHTML = "<ul>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li data-type="city" data-walkername="${walker.name}" >${walker.city}</li>`
+    }
+
+    citiesHTML += "</ul>"
+
+    return citiesHTML
+}
+
---
+++ scripts/Walkers.js
@@ -0,0 +1,30 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click", 
+    (theClickEvent) => {
+        const whatWasClickedOn = theClickEvent.target
+        
+        if (whatWasClickedOn.dataset.type === "walker") {
+            window.alert(`This walker works in ${whatWasClickedOn.dataset.city}`)
+        }
+    }
+)
+
+export const Walkers = () => {
+    let walkerHTML = "<ul>"
+
+    for (const walker of walkers) {
+        walkerHTML += `<li data-id="${walker.id}"
+                            data-city="${walker.city}"
+                            data-type="walker"
+                            >${walker.name}
+                       </li>`
+    }
+
+    return walkerHTML += "</ul>"
+
+}
+
